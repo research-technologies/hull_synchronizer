8d22b3db93e0671b344c291e8e61fca57fe31c25
Anusha Ranganathan <anusha3@gmail.com>
2018-05-31 03:58:06 +0100

    CRUS operations for data mapper

---
+++ app/controllers/data_mappers_controller.rb
@@ -0,0 +1,45 @@
+class DataMappersController < ApplicationController
+  def index
+    @data_mappers = DataMapper.order('created_at')
+  end
+
+  # New action for creating a new data mapper record
+  def new
+    @data_mapper = DataMapper.new
+  end
+
+  def create
+    @data_mapper = DataMapper.new(data_mapper_params)
+    if @data_mapper.save
+      flash[:notice] = "Successfully uploaded csv file. the file is being processed. <br/>
+        You can download the reformatted file once complete"
+      redirect_to @data_mapper
+    else
+      flash[:alert] = "Error uploading cdv file!"
+      render :new
+    end
+  end
+
+  def show
+    @data_mapper = DataMapper.find(params[:id])
+  end
+
+  #Destroy action for deleting an already uploaded file
+  def destroy
+    @data_mapper = DataMapper.find(params[:id])
+    if @data_mapper.destroy
+      flash[:notice] = "Successfully deleted csv record!"
+      redirect_to action: "index"
+    else
+      flash[:alert] = "Error deleting csv record"
+    end
+  end
+
+  private
+
+  #Permitted parameters when uploading a file. This is used for security reasons.
+  def data_mapper_params
+    params.require(:data_mapper).permit(:title, :file_type, :original_file)
+  end
+
+end
