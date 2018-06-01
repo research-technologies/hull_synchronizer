284b24716c47f3c9edba3341f3556829367f0e1f
Anusha Ranganathan <anusha3@gmail.com>
2018-06-02 00:34:44 +0100

    Install and configure sidekiq and redis

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'data_mappers#index'
   resources :data_mappers
+
+  require 'sidekiq/web'
+  mount Sidekiq::Web => '/sidekiq'
 end
