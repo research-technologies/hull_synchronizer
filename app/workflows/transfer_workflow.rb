3525c0d6c58951c9eafef592c2173dff3df4dfaf
Anusha Ranganathan <anusha3@gmail.com>
2018-10-25 20:33:00 +0100

    Make number of files readable

--- app/workflows/transfer_workflow.rb
+++ app/workflows/transfer_workflow.rb
@@ -1,8 +1,11 @@
 class TransferWorkflow < Gush::Workflow
+  attr_reader :number_of_files
 
   # @params params [Hash] supply any params needed to start the first job
   def configure(params)
-    params.fetch(:item_list, {}).each do |file_id, relative_path|
+    item_list = params.fetch(:item_list, {})
+    @number_of_files = item_list.size
+    item_list.each do |file_id, relative_path|
       job_params = {
         item_id: file_id,
         relative_path: relative_path
