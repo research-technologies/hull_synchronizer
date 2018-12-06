8e629f1b822e9d481fd6fc4297f0ab1c336d6981
Julie Allinson <julie.allinson@london.ac.uk>
2018-12-06 15:37:34 +0000

    Add CalmJob into workflow, via DepositJob

--- app/jobs/sword/deposit_job.rb
+++ app/jobs/sword/deposit_job.rb
@@ -2,7 +2,7 @@ module Sword
   # SWORD deposit job
   class DepositJob < BaseJob
     attr_reader :sword_api
-    
+
     # Prepare and perform a single SWORD deposit
     def perform
       setup_sword
@@ -16,15 +16,21 @@ module Sword
         @sword_api = Sword::Api::Work.new(params: payloads.first[:output][:works][params])
       end
 
+      # run the CalmJob if the deposit succeeds
       def act_on_ok
+        act_on_calm
+      end
+
+      def act_on_calm
+        calm_job = CalmJob.perform(sword_api.deposit[:content].split('/').last)
         @message_text = "#{sword_api.deposit[:content].split('/').last} successfully deposited"
         output(
-          event: 'success',
-          message: message_text,
+          event: calm_job[:event],
+          message: "#{message_text}; #{calm_job[:message]}",
           package_id: payloads.first[:output][:package_id],
-          work_id: sword_api.deposit[:content].split('/').last,
           works: payloads.first[:output][:works]
         )
+        fail! if calm_job[:event] == 'failed'
       end
   end
 end
--- spec/jobs/calm/calm_job_spec.rb
+++ spec/jobs/calm/calm_job_spec.rb
@@ -1,31 +1,35 @@
 RSpec.describe Calm::CalmJob do
-  let(:subject) { described_class.new }
+  let(:calm_job) { described_class.new }
   let(:calm_api) { instance_double(Calm::Api) }
-  let(:metadata_incoming) { { calm_metadata: { reference: 'Collection', filename: 'my file', accession_number: 'abc' } } }
+  let(:metadata_incoming) do
+    { calm_metadata: {
+      reference: 'Collection',
+      filename: 'my file',
+      accession_number: 'abc'
+    } }
+  end
 
   describe 'successful job' do
     before do
-      allow(subject).to receive(:setup_calm)
+      allow(calm_job).to receive(:setup_calm)
       allow(calm_api).to receive(:get_record_by_field).with('RefNo', 'Collection').and_return([true, { 'RecordID' => ['12345'] }])
       allow(calm_api).to receive(:create_child_record).and_return([true, '12345'])
 
-      subject.calm_api = calm_api
-      subject.params = 0
-      subject.payloads = [
+      calm_job.calm_api = calm_api
+      calm_job.params = 0
+      calm_job.payloads = [
         { output: {
-          work_id: 'hyrax_work',
           works: [metadata_incoming]
         } }
       ]
     end
     context 'with only mandatory metdata' do
       it 'performs the job and sets the payload for the next job' do
-        expect(subject).to receive(:output).with(event: 'success', message: '12345 successfully added to CALM', works: [metadata_incoming])
-        subject.perform
+        expect(calm_job.perform('hyrax_work')).to eq(event: 'success', message: '12345 successfully added to CALM')
       end
       it 'contains default values for language and access_status, and the hyrax_work id' do
-        subject.perform
-        expect(subject.fields).to eq("Title" => "File name: my file", "AccNo" => "abc", "AccessStatus" => "closed", "Language" => "English", "Location" => "hyrax_work")
+        calm_job.perform('hyrax_work')
+        expect(calm_job.fields).to eq("Title" => "File name: my file", "AccNo" => "abc", "AccessStatus" => "closed", "Language" => "English", "Location" => "hyrax_work")
       end
     end
 
@@ -33,30 +37,28 @@ RSpec.describe Calm::CalmJob do
       let(:metadata_incoming) { { calm_metadata: { reference: 'Collection', filename: 'my file', accession_number: 'abc', title: 'Title', language: 'German', access_status: 'open', description: 'description', user_description: 'user description' } } }
 
       it 'contains the supplied values, not the defaults' do
-        subject.perform
-        expect(subject.fields).to eq("Title" => "Title", "AccNo" => "abc", "AccessStatus" => "open", "Description" => "description\nUser Description: description", "Language" => "German", "Location" => "hyrax_work")
+        calm_job.perform('hyrax_work')
+        expect(calm_job.fields).to eq("Title" => "Title", "AccNo" => "abc", "AccessStatus" => "open", "Description" => "description\nUser Description: description", "Language" => "German", "Location" => "hyrax_work")
       end
     end
   end
 
   describe 'unsuccessful job, do not retry' do
     before do
-      allow(subject).to receive(:setup_calm)
+      allow(calm_job).to receive(:setup_calm)
       allow(calm_api).to receive(:get_record_by_field).with('RefNo', 'Collection').and_return([false, 'An error!'])
       allow(calm_api).to receive(:create_child_record).and_return([false, 'Another error!'])
 
-      subject.calm_api = calm_api
-      subject.params = 0
-      subject.payloads = [
+      calm_job.calm_api = calm_api
+      calm_job.params = 0
+      calm_job.payloads = [
         { output: {
-          work_id: 'hyrax_work',
           works: [metadata_incoming]
         } }
       ]
     end
     it 'performs the job and sets the payload for the next job' do
-      expect(subject).to receive(:output).with(event: 'failed', message: 'Job failed with: Another error!')
-      subject.perform
+      expect(calm_job.perform('hyrax_work')).to eq(event: 'failed', message: 'Job failed with: Another error!')
     end
   end
 end
