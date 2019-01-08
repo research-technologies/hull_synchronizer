a591c69af37e712a06d439b8517e7413ee469c3d
Anusha Ranganathan <anusha3@gmail.com>
2019-01-08 14:41:25 +0000

    First pass at dockerizing

---
+++ docker/db/docker-entrypoint-initdb.d/hyrax.sql
@@ -0,0 +1,3 @@
+CREATE DATABASE hull_sync_dev;
+CREATE DATABASE hull_sync_test;
+CREATE DATABASE hull_sync_production;
---
+++ hull-sync-docker-entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "Creating log folder"
+mkdir -p $APP_WORKDIR/log
+
+if [ "$RAILS_ENV" = "production" ]; then
+    # Verify all the production gems are installed
+    bundle check
+else
+    # install any missing development gems (as we can tweak the development container without rebuilding it)
+    bundle check || bundle install --without production
+fi
+
+## Run any pending migrations
+bundle exec rake db:migrate
+
+echo "--------- Starting Hyrax in $RAILS_ENV mode ---------"
+sleep 2000
+# rm -f /tmp/hull_sync.pid
+# bundle exec rails server -p 3000 -b '0.0.0.0' --pid /tmp/hull_sync.pid
