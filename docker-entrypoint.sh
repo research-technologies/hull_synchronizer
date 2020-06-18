fa771fccdfd47b69d91d37bba5addb9f39b6bc6c
Julie Allinson <julie.allinson@cosector.onmicrosoft.com>
2020-06-18 17:54:37 +0000

    updated docker-entrypoint for certbot

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -11,9 +11,9 @@ mkdir -p $LOCAL_BOX_DIR $LOCAL_EFS_DATA_DIR $LOCAL_EFS_TRANSFER_DIR /data/dipsto
 
 echo "------------- installing certbot -----------"
 
-add-apt-repository ppa:certbot/certbot -y
-apt-get update
-apt-get install python-certbot-apache -y --no-install-recommends
+add-apt-repository ppa:certbot/certbot -y -q
+apt-get -qq update
+apt-get install python-certbot-apache -qq -y --no-install-recommends
 
 #Copy in certbot config
 cp $APP_WORKDIR/docker/cli.ini /etc/letsencrypt/cli.ini
@@ -28,17 +28,48 @@ mkdir -p /var/www/acme-docroot/.well-known/acme-challenge
 sed -i "s/#SERVER_NAME#/$HULLSYNC_SERVER_NAME/" /etc/apache2/sites-available/hullsync.conf
 sed -i "s/#SERVER_NAME#/$HULLSYNC_SERVER_NAME/" /etc/apache2/sites-available/hullsync_ssl.conf
 
-echo "--------- Starting Apache -----------"
-service apache2 start
+if [ -z "$DOCKER_UP_BUILD"  ]; then 
 
-echo "-------------- Getting cert(s) -----------"
+	echo "--------- Starting Apache -----------"
+	service apache2 start
 
-# We'll register each time as certs are not stored on a persistent volume
-certbot register
-certbot certonly -n --cert-name base -d $HULLSYNC_SERVER_NAME
+        if [ -f /data/pki/$APP_KEY/letsencrypt/live/base/fullchain.pem ]; then
 
-# copy autorenewal script. Dest directory only exists after the first cert is in place
-cp $APP_WORKDIR/docker/00_apache2 /etc/letsencrypt/renewal-hooks/deploy/
+                echo "We should already have a cert so lets not encrypt at this precise moment...."
+                mkdir -p /etc/letsencrypt #not sure this step needed anymore...
+                cp -r /data/pki/$APP_KEY/letsencrypt/* /etc/letsencrypt/
+        else
+                echo "-------------## Getting cert(s) ##----------"
+
+                # We'll register each time as certs are not stored on a persistent volume
+                certbot register
+		certbot certonly -n --cert-name base -d $HULLSYNC_SERVER_NAME -d www.$HULLSYNC_SERVER_NAME
+
+                # copy autorenewal script. Dest directory only exists after the first cert is in place
+        fi
+
+        cp $APP_WORKDIR/docker/00_apache2 /etc/letsencrypt/renewal-hooks/deploy/
+
+        if [ ! -d /data/pki/$APP_KEY/letsencrypt ]; then
+            echo "copy the certs for later use"
+            mkdir /data/pki/$APP_KEY #in case
+            cp -rL /etc/letsencrypt /data/pki/$APP_KEY/
+        fi
+
+	echo "--------- Restarting Apache with ssl and real cert ---------"
+else
+
+        echo "--------- Starting Apache with ssl (ignoring cert domain name for local docker up) ---------"
+        mkdir -p /etc/apache2/ssl/certs
+        mkdir -p /etc/apache2/ssl/private
+        ln -s $APP_WORKDIR/vendor/$GEM_KEY/pki/$SSL_CERT /etc/apache2/ssl/certs/$SSL_CERT
+        ln -s $APP_WORKDIR/vendor/$GEM_KEY/pki/$SSL_KEY /etc/apache2/ssl/private/$SSL_KEY
+        ln -s $APP_WORKDIR/vendor/$GEM_KEY/pki/$SSL_CA /etc/apache2/ssl/certs/$SSL_CA
+
+        sed -i "s#/etc/letsencrypt/live/base/fullchain.pem#/etc/apache2/ssl/certs/$SSL_CERT#" /etc/apache2/sites-available/hyrax_ssl.conf
+        sed -i "s#/etc/letsencrypt/live/base/privkey.pem#/etc/apache2/ssl/private/$SSL_KEY#" /etc/apache2/sites-available/hyrax_ssl.conf
+
+fi
 
 echo "--------- Restarting Apache with ssl ---------"
 a2ensite hullsync_ssl
