dcbdf913cf39db2eee472873c81efe10ac85cfb8
Rory McNicholl <rory.mcnicholl@london.ac.uk>
2021-12-03 11:13:13 +0000

    probably flawed attenpt to stop HS sidekiq pos attempting to do hyrax jobs

---
+++ app/jobs/ingest_job.rb
@@ -0,0 +1,12 @@
+# RM: This is here just to stop errors that occur because we are recieving job 
+# requests (IngestJob and CreateDerivativesJob) from hyrax to the hullsyncsidekiq 
+# for some reason. This will stop name errors and hopefully not mess up the post 
+# ingest processing in hyrax which will be handled by the sidekiq pod
+
+class IngestJob < ActiveJob::Base
+  queue_as :default
+  
+  def perform(*args)
+  end
+  
+end
