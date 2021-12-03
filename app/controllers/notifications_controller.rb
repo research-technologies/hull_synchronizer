9af1dfdd0b83023c60d7d5780eb9a65cba863878
Rory McNicholl <rory.mcnicholl@london.ac.uk>
2021-12-03 10:30:28 +0000

    remove comment/debug

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,15 +1,33 @@
 class NotificationsController < ApplicationController
   protect_from_forgery with: :null_session
   def index
-    logger.info("---------------------------")
-    logger.info("BOX NOTIFICATION RECEIVED")
-    logger.info("Event:\t#{params[:event_type]}")
-    logger.info("Item Name:\t#{params[:item_name]}")
-    logger.info("Item Type:\t#{params[:item_type]}")
-    logger.info("Item Id:\t#{params[:item_id]}")
     if params[:event_type] == 'added_collaborator'
-      TransferWorkflowManager.new(params: params)
+      prefix = "#{params[:item_id].gsub(/[^0-9A-Z]/i, '_')}_"
+      STDERR.puts "tmp file prefix: #{prefix}"
+      files=Dir["/tmp/#{prefix}timestamp"]
+      if files.empty?
+        tmp_file=make_tmp_file(prefix)
+        TransferWorkflowManager.new(params: params)
+      else
+        check_timestamp(files.first,prefix)
+        STDERR.puts("---- IGNORING ADD COLLAB, TOO SOON! ----")
+      end
     end
-    render json:{'note': "Box Notification Received"}
   end
+
+  def make_tmp_file(prefix)
+    File.open("/tmp/#{prefix}timestamp", 'w') {|f| f.write(Time.now.utc) }
+  end
+
+  def check_timestamp(file,prefix)
+    timestamp = DateTime.parse(File.read(file))
+    STDERR.puts "timestamp: #{timestamp}, timestamp.utc #{timestamp.utc}, 2 hours ago: #{(Time.now - 120.minutes).utc}"
+    if timestamp.utc < (Time.now - 120.minutes).utc
+      TransferWorkflowManager.new(params: params)
+      # refresh expired timestamp file
+      File.delete(file) if File.exist?(file)
+      tmp_file=make_tmp_file(prefix)
+    end
+  end
+
 end
