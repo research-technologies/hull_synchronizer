1aa271d5291cf3a9be8c284b40e70c8a215ca044
Rory McNicholl <rory.mcnicholl@london.ac.uk>
2024-02-12 10:31:53 +0000

    remove thread_safe for newer redis

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -2,7 +2,9 @@
 
 config = YAML.safe_load(ERB.new(IO.read(Rails.root + 'config' + 'redis.yml')).result)[Rails.env].with_indifferent_access
 # added nil https://github.com/mperham/sidekiq/wiki/Using-Redis
-redis_config = config.merge(thread_safe: true, id: nil)
+#redis_config = config.merge(thread_safe: true, id: nil)
+redis_config = config.merge(id: nil)
+
 
 Sidekiq::Logging.logger.level = Logger::WARN if ENV['RAILS_ENV'] == 'production'
 
